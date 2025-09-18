1a29c4cda2b2066de6c05ca9bfac32f2d0eae93c
Student BE106 <Student.BE106@chaffey.edu>
2025-09-18 16:02:04 -0700

    sync push

--- jarofpaperstarlight/Scripts/player.gd
+++ jarofpaperstarlight/Scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 signal hit
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
-var jump_speed = 200.0
+var jump_speed = -700.0
 var screen_size # Size of the game window.
 var jumping = false
 #var delta = 0;
@@ -12,13 +12,13 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
 	screen_size = get_viewport_rect().size
 	
-func _physics_process(delta):
-	#velocity.y += gravity * delta # adds gravity
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		#velocity.y = velocity.y - (gravity * delta)
-		#velocity.y += jump_speed
-		velocity.y += gravity * delta
+#func _physics_process(delta):
+#	#velocity.y += gravity * delta # adds gravity
+#	# Vertical Velocity
+#	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+#		#velocity.y = velocity.y - (gravity * delta)
+#		#velocity.y += jump_speed
+#		velocity.y += gravity * delta
 	
 	#if jumping:
 	#	if ()
@@ -29,12 +29,12 @@ func _physics_process(delta):
 	#	print("I collided with ", collision.get_collider().name)
 
 	# Using move_and_slide.
-	move_and_slide()
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
+#	move_and_slide()
+#	for i in get_slide_collision_count():
+#		var collision = get_slide_collision(i)
 		#print("I collided with ", collision.get_collider().name)	
-	
-func _process(delta):
+	# _process(delta)
+func  _physics_process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -44,8 +44,8 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up") and is_on_floor():
 		#velocity.y -= 1
-		velocity.y -= jump_speed * delta
-		position.y -= jump_speed 
+		velocity.y = jump_speed #* delta
+		#position.y -= jump_speed 
 		jumping = true;
 	#else:
 	#	jumping = false
@@ -65,9 +65,19 @@ func _process(delta):
 		$AnimatedSprite2D.animation = "Up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 		
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		#velocity.y = velocity.y - (gravity * delta)
+		#velocity.y += jump_speed
+		velocity.y += gravity * delta
+		
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-
+	
+	#position = position.clamp(Vector2.ZERO, screen_size)
+	# Using move_and_slide.
+	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		#print("I collided with ", collision.get_collider().name)	
 
 func start(pos):
 	position = pos
